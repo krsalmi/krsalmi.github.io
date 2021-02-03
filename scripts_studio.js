9f616833e5cc9bd075ad5cc4f6dc9a76a05ff8e1
Kris Salmi <kristiina.k.salmi@gmail.com>
2021-02-03 23:34:15 +0200

    Added functions for animated underlines of links, fade-in/fade-out of carousel arrows, changing bg color of nav menu,  as well ass css snap for influencing page scrolling.

--- scripts_studio.js
+++ scripts_studio.js
@@ -1,15 +1,58 @@
+/* These two functions control the (fade-in/fade-out displaying) of the 
+'carousel-arrows' which are situated on each side of the carousel images.
+The 'else' case in the first function is for when an image is changed
+automatically by the slideshow. In this case, the display:block has to 
+happen with a slight delay, otherwise it is overturned by the original
+display:none in the 'carousel-arrows'.
+*/
+
+function displaySlowlyOnHover(from_pic) {
+    const element = document.getElementById('carousel-arrows');
+    if (from_pic  === 0) {
+        element.classList.remove("fade-out-div");
+        element.classList.add("fade-in-div");
+        element.style.display = "block";
+    } else {
+        console.log("from pic");
+        setTimeout(function(){
+            element.style.display = "block";
+        }, 500);
+    }
+}
+
+function hideElement() {
+    const element = document.getElementById('carousel-arrows');
+    element.classList.remove("fade-in-div");
+    element.classList.add("fade-out-div");
+    setTimeout(function() {
+    element.style.display = "none";
+    //console.log("im hiding and timer");
+   }, 500);
+}
+
+/* Inspired by W3.School
+Image carousel slideshow with automatic as well as manual changing.
+When the image changes, if the cursor is on the 'carousel' div,
+it will send to 'displaySlowlyOnHover' as the special 'else' case.
+*/
 
  function curPic(n) {
      clearTimeout(timer);
      displayPics(index = n);
  }
 
+ function nextPic(n) {
+    clearTimeout(timer);
+    displayPics(index += n);
+  }
+
  function displayPics(n) {
      var i;
      var slides = document.getElementsByClassName("pics");
      var dots = document.getElementsByClassName("dot");
+     var carousel = document.getElementsByClassName("carousel");
      if (n==undefined){
-         n = ++index;
+        n = ++index;
      }
      if (n > slides.length) {
          index = 1;
@@ -25,5 +68,30 @@
      }
      slides[index-1].style.display = "block";
      dots[index-1].className += " active";
-     timer = setTimeout(displayPics, 5000);
+     timer = setTimeout(displayPics, 3500);
+
+     var carousel = document.getElementById("carousel");
+     var arrows = document.getElementById("carousel-arrows")
+     carousel.addEventListener("mouseover", displaySlowlyOnHover);
  }
+
+ /* This function checks to see if any of the 'sections' in the 'section_list'
+ is displayed at the top of the window. If so, it searches for the second child of
+ that 'selection' (which is the .under_navbar div) and changes its background color,
+ because otherwise the see-through menu bar wouldn't be properly visible. If the
+ element is not at the top of the screen, its child div will be white.
+ */
+
+ function changeColorUnderNavbar() {
+    var section_list = ["sect_services", "sect_team", "sect_contact"];
+    section_list.forEach(checkPos);
+    function checkPos(item){
+        var sect_item = document.getElementById(item);
+        var under_nav = sect_item.childNodes;
+        if (sect_item.getBoundingClientRect().top == 0) {
+            under_nav[1].style.backgroundColor = "#A30000";
+        } else {
+            under_nav[1].style.backgroundColor = 'white';
+        }
+    }
+}
